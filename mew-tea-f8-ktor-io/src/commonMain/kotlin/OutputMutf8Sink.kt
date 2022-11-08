b06002f60949df34ecc1f6dfc1565a6fa1f5daaa
Nullicorn <bennullicorn@gmail.com>
2022-11-08 15:16:27 -0500

    Fix `Mutf8Sink`s throwing the wrong exceptions on non-JVM platforms
    - Same issue as fb45479, but for `Mutf8Sink` implementations instead of `Mutf8Source`

--- mew-tea-f8-ktor-io/src/commonMain/kotlin/OutputMutf8Sink.kt
+++ mew-tea-f8-ktor-io/src/commonMain/kotlin/OutputMutf8Sink.kt
@@ -1,6 +1,8 @@
 package me.nullicorn.mewteaf8
 
 import io.ktor.utils.io.core.*
+import io.ktor.utils.io.errors.IOException as KtorIOException
+import me.nullicorn.mewteaf8.IOException as Mutf8IOException
 
 /**
  * A [Mutf8Sink] that writes its data to an [Output] from the ktor library.
@@ -13,17 +15,29 @@ import io.ktor.utils.io.core.*
  */
 class OutputMutf8Sink(private val output: Output, bytesPerWrite: Int = 1024) : Mutf8Sink(bytesPerWrite) {
 
-    override fun writeLength(mutf8Length: Int) {
-        require(mutf8Length >= 0) { "mutf8Length must be at least 0, not $mutf8Length" }
-        require(mutf8Length <= 65535) { "mutf8Length must be at most 65535, not $mutf8Length" }
+    override fun writeLength(mutf8Length: Int): Unit =
+        tryOrThrowMutf8Exception {
+            require(mutf8Length >= 0) { "mutf8Length must be at least 0, not $mutf8Length" }
+            require(mutf8Length <= 65535) { "mutf8Length must be at most 65535, not $mutf8Length" }
 
-        output.writeShort(mutf8Length.toShort(), byteOrder = ByteOrder.BIG_ENDIAN)
-    }
+            output.writeShort(mutf8Length.toShort(), byteOrder = ByteOrder.BIG_ENDIAN)
+        }
 
-    override fun writeBytes(bytes: ByteArray, untilIndex: Int) {
-        require(untilIndex >= 0) { "untilIndex must be at least 0, not $untilIndex" }
-        require(untilIndex <= bytes.size) { "untilIndex, $untilIndex, must not exceed bytes.size, ${bytes.size}" }
+    override fun writeBytes(bytes: ByteArray, untilIndex: Int): Unit =
+        tryOrThrowMutf8Exception {
+            require(untilIndex >= 0) { "untilIndex must be at least 0, not $untilIndex" }
+            require(untilIndex <= bytes.size) { "untilIndex, $untilIndex, must not exceed bytes.size, ${bytes.size}" }
 
-        output.writeFully(bytes, offset = 0, length = untilIndex)
-    }
+            output.writeFully(bytes, offset = 0, length = untilIndex)
+        }
+
+    @Suppress("CAST_NEVER_SUCCEEDS")
+    private inline fun <T> tryOrThrowMutf8Exception(block: () -> T): T =
+        try {
+            block()
+
+        } catch (cause: KtorIOException) {
+            throw cause as? Mutf8IOException
+                ?: Mutf8IOException(cause.message ?: "An I/O issue occurred while writing", cause)
+        }
 }
--- mew-tea-f8-okio/src/commonMain/kotlin/OkioMutf8Sink.kt
+++ mew-tea-f8-okio/src/commonMain/kotlin/OkioMutf8Sink.kt
@@ -3,6 +3,8 @@ package me.nullicorn.mewteaf8
 import okio.BufferedSink
 import okio.Sink
 import okio.buffer
+import me.nullicorn.mewteaf8.IOException as Mutf8IOException
+import okio.IOException as OkioIOException
 
 /**
  * A [Mutf8Sink] that writes its data to a [Sink] from the okio library.
@@ -17,17 +19,29 @@ class OkioMutf8Sink(sink: Sink, bytesPerWrite: Int = 1024) : Mutf8Sink(bytesPerW
 
     private val sink: BufferedSink = (sink as? BufferedSink) ?: sink.buffer()
 
-    override fun writeLength(mutf8Length: Int) {
-        require(mutf8Length >= 0) { "mutf8Length must be at least 0, not $mutf8Length" }
-        require(mutf8Length <= 65535) { "mutf8Length must be at most 65535, not $mutf8Length" }
+    override fun writeLength(mutf8Length: Int): Unit =
+        tryOrThrowMutf8Exception {
+            require(mutf8Length >= 0) { "mutf8Length must be at least 0, not $mutf8Length" }
+            require(mutf8Length <= 65535) { "mutf8Length must be at most 65535, not $mutf8Length" }
 
-        sink.writeShort(mutf8Length)
-    }
+            sink.writeShort(mutf8Length)
+        }
 
-    override fun writeBytes(bytes: ByteArray, untilIndex: Int) {
-        require(untilIndex >= 0) {"untilIndex must be at least 0, not $untilIndex"}
-        require(untilIndex <= bytes.size) {"untilIndex, $untilIndex, must not exceed bytes.size, ${bytes.size}"}
+    override fun writeBytes(bytes: ByteArray, untilIndex: Int): Unit =
+        tryOrThrowMutf8Exception {
+            require(untilIndex >= 0) { "untilIndex must be at least 0, not $untilIndex" }
+            require(untilIndex <= bytes.size) { "untilIndex, $untilIndex, must not exceed bytes.size, ${bytes.size}" }
 
-        sink.write(bytes, offset = 0, byteCount = untilIndex)
-    }
+            sink.write(bytes, offset = 0, byteCount = untilIndex)
+        }
+
+    @Suppress("CAST_NEVER_SUCCEEDS")
+    private inline fun <T> tryOrThrowMutf8Exception(block: () -> T): T =
+        try {
+            block()
+
+        } catch (cause: OkioIOException) {
+            throw cause as? Mutf8IOException
+                ?: Mutf8IOException(cause.message ?: "An I/O issue occurred while writing", cause)
+        }
 }
